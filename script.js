d4b30552b3a511f27ab37fb07127ea647ad9b1cd
Fei <Fei@gmail.com>
2024-04-12 01:17:01 +0800

    complete version

---
+++ script.js
@@ -0,0 +1,23 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slided-images");
+  let dots = document.getElementsByClassName("circle");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+
+menu = document.querySelector('.menu');
+mainNav = document.querySelector('.main-nav');
+menu.addEventListener('click', () => mainNav.classList.toggle('active'));
